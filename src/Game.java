234c079e49b1c835aec8d86b7844e479314c5dc4
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-05-14 13:31:37 -0300

    aaaa

---
+++ src/Game.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+
+public class Game extends Canvas implements ActionListener, KeyListener {
+    private final int FATNESS = 15; // size in pixels^2
+    private final int INITIAL_LENGTH = 5;
+    private Snake s = new Snake(INITIAL_LENGTH, FATNESS);
+
+    private Game(){
+        this.addKeyListener(this);
+        Timer timer = new Timer(100, this);
+        timer.setRepeats(true);
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Game");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Canvas canvas = new Game();
+        canvas.setBackground(Color.BLACK);
+        canvas.setSize(800, 600);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.green);
+        for(int[] i: s.parts){
+            g.fillRect(i[0], i[1], s.FATNESS, s.FATNESS);
+        }
+        s.move_forward(); //move forward on every render (direction is based on Snake.FACING)
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_UP:
+                if(s.FACING != 'S') { s.FACING = 'N'; }
+                break;
+            case KeyEvent.VK_DOWN:
+                if(s.FACING != 'N') { s.FACING = 'S'; }
+                break;
+            case KeyEvent.VK_LEFT:
+                if(s.FACING != 'E') { s.FACING = 'W'; }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if(s.FACING != 'W') { s.FACING = 'E'; }
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ src/Snake.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Snake {
+    List<int[]> parts = new ArrayList<>();
+    int FATNESS;
+    char FACING = 'S';
+
+    Snake(int length, int fatness){
+        FATNESS = fatness;
+        for(int i = 0; i < length; i++){
+            parts.add(new int[]{fatness*i, 0});
+        }
+    }
+
+    void move_forward(){
+
+        parts.remove(0); //remove the tail
+
+        //append head based on the direction that the snake's facing
+        int[] head_pos = parts.get(parts.size()-1);
+        switch(FACING){
+            case 'N': //up
+                parts.add(new int[]{head_pos[0], head_pos[1]-FATNESS});
+                break;
+            case 'S': //down
+                parts.add(new int[]{head_pos[0], head_pos[1]+FATNESS});
+                break;
+            case 'E': //right
+                parts.add(new int[]{head_pos[0]+FATNESS, head_pos[1]});
+                break;
+            case 'W': //left
+                parts.add(new int[]{head_pos[0]-FATNESS, head_pos[1]});
+                break;
+        }
+    }
+}
